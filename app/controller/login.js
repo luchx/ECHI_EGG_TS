47cf861b5ab97fe922767bdbdace4b03029870ad
Echi <914743765@qq.com>
2019-03-19 00:18:59 +0800

    修改获取验证码参数传递方式

--- app/controller/login.js
+++ app/controller/login.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-function TestPhone(phone) {
-  let reg = /^((13[0-9])|(15[^4])|(166)|(17[0-8])|(18[0-9])|(19[8-9])|(14[5|7]))\d{8}$/;
-  return reg.test(phone);
-}
+let { TestPhone } = require('../utils/validator');
 class LoginController extends Controller {
   async getCode() {
     const {
@@ -12,7 +9,7 @@ class LoginController extends Controller {
     } = this;
     let {
       phone
-    } = ctx.params;
+    } = ctx.query;
     let result = {
       code: 0,
       message: '请求成功',
@@ -22,9 +19,8 @@ class LoginController extends Controller {
       result['code'] = 500001;
       result['message'] = '请输入您的手机号码';
       result['data'] = null;
-    }
-    if (!TestPhone(phone)) {
-      result['code'] = 500001;
+    }else if (!TestPhone(phone)) {
+      result['code'] = 500002;
       result['message'] = '您的号码输入错误';
       result['data'] = null;
     }
