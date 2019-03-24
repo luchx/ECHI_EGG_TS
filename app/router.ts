7e25d9fb4b88beae7630541a97efcfd274a181b7
Echi <914743765@qq.com>
2019-03-24 11:03:14 +0800

    删除

--- app/router.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from 'egg';
-
-export default (app: Application) => {
-  const { controller, router } = app;
-  // const apiPrefix = app.config.apiPrefix;
-
-  router.get('/', controller.home.index);
-};
