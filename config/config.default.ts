f4ec65525b027a314786e1a460b006dd5b840ac1
luchx <wb-lcx595275@antfin.com>
2019-09-02 18:49:50 +0800

    feat: 添加跨域请求

--- config/config.default.ts
+++ config/config.default.ts
@@ -74,17 +74,17 @@ export default (appInfo: EggAppInfo) => {
   };
 
   // 配置跨域
-  // config.security = {
-  //   csrf: {
-  //     enable: false,
-  //     ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
-  //   },
-  //   domainWhiteList: [ 'http://localhost:3001' ],
-  // };
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    },
+    domainWhiteList: [ 'http://localhost:1993' ],
+  };
 
-  // config.cors = {
-  //   allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
-  // };
+  config.cors = {
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  };
 
   // the return config will combines to EggAppConfig
   return config;
