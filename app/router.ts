9babd2d2f594816a35c690260b717a978f153b75
luchx <luchx1993@gmail.com>
2019-09-08 20:53:58 +0800

    fix: typings

--- app/router.ts
+++ app/router.ts
@@ -1,15 +1,15 @@
 import { Application } from 'egg';
 
 export default (app: Application) => {
-  const { controller, router } = app;
+  const { controller } = app;
 
-  router.get('/', controller.home.index);
-  router.get('/member/getTime', controller.member.getTime);
-  router.get('/member/getCode', controller.member.getCode);
-  router.post('/member/login', controller.member.login);
-  router.get('/member/getInfo/:memberId', controller.member.getInfo);
-  router.put('/member/modify', controller.member.modify);
+  app.get('/', controller.home.index);
+  app.get('/member/getTime', controller.member.getTime);
+  app.get('/member/getCode', controller.member.getCode);
+  app.post('/member/login', controller.member.login);
+  app.get('/member/getInfo/:memberId', controller.member.getInfo);
+  app.put('/member/modify', controller.member.modify);
 
-  router.post('/project/create', controller.project.create);
-  router.get('/project/getProject', controller.project.getProject);
+  app.post('/project/create', controller.project.create);
+  app.get('/project/getProject', controller.project.getProject);
 };
