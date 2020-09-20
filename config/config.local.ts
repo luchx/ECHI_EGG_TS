f550a573772e6cb556daa716c4dd0f9ec69d08cb
luchx <luchx1993@gmail.com>
2020-09-20 21:27:42 +0800

    🎨 删除无用文件

--- config/config.local.ts
+++ config/config.local.ts
@@ -9,7 +9,7 @@ export default () => {
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'root',
+    password: '123456',
   };
 
   return config;
--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -1,13 +1,17 @@
-// This file is created by egg-ts-helper@1.25.6
+// This file is created by egg-ts-helper@1.25.8
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+type AnyClass = new (...args: any[]) => any;
+type AnyFunc<T = any> = (...args: any[]) => T;
+type CanExportFunc = AnyFunc<Promise<any>> | AnyFunc<IterableIterator<any>>;
+type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? ReturnType<T> : T> = U extends AnyClass ? InstanceType<U> : U;
 import ExportMember from '../../../app/service/member';
 import ExportProject from '../../../app/service/project';
 
 declare module 'egg' {
   interface IService {
-    member: ExportMember;
-    project: ExportProject;
+    member: AutoInstanceType<typeof ExportMember>;
+    project: AutoInstanceType<typeof ExportProject>;
   }
 }
